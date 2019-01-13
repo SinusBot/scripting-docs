1d9af5541546d1281c36e1f67e66524cd3849cc8
Jonas Bögle <Jonas@sandstorm-projects.de>
2019-01-13 20:53:30 +0100

    added fs module

--- api.js
+++ api.js
@@ -2543,8 +2543,105 @@ class Graphics {
      * }, function() { engine.setAvatarFromBanner('banner.png'); });
      * @param {string} filename - filename without ending
      * @param {object} graphicsSettings
-     * @param {Function} callback
+     * @param {function} callback
      * @return {boolean} success
      */
     setBanner(filename, graphicsSettings, callback) { }
+}
+
+/**
+ * @class
+ * @mixin
+ */
+class FileInfo {
+    /**
+     * @returns {string} Base name of the file
+     */
+    name() { }
+    /**
+     * @returns {string} Length in bytes for regular files; system-dependent for others
+     */
+    size() { }
+    // FIXME: Either document FileMode or change to number
+    /**
+     * @returns {FileMode} File mode bits
+     */
+    mode() { }
+    /**
+     * @returns {Date} Modification time
+     */
+    modTime() { }
+    /**
+     * @returns {boolean} Returns if the file is a directory
+     */
+    isDir() { }
+    // FIXME: What is returned here exactly?
+    // /**
+    //  * @returns {object} Underlying data source (can return nil)
+    //  */
+    // sys() { }
+}
+
+/**
+ * @class
+ * @mixin
+ */
+class FS {
+    /**
+     * @description Checks if a file exists
+     * @param {string} path - Path to the file (for example `/var/www/html/foo.txt`)
+     * @return {boolean}
+     */
+    exists(path) { }
+    /**
+     * @description Reads the content of a file
+     * @param {string} path - Path to the file
+     * @return {Bytes}
+     */
+    readFile(path) { }
+    /**
+     * @description Writes data to a file
+     * @param {string} path - Path to the file
+     * @param {BytesWriter} data - Data as Bytes, e.g. created by `helpers.BytesFromString()` or similar functions
+     * @param {number} mode - File mode bits
+     * @return {boolean} success
+     */
+    writeFile(path, data, mode) { }
+    /**
+     * @description Returns information about a file
+     * @param {string} path - Path to the file
+     * @return {FileInfo} FileInfo
+     */
+    stat(path) { }
+    /**
+     * @description Returns information about all files in a dirictory
+     * @param {string} path - Path to the directory
+     * @return {FileInfo[]} Array of FileInfo
+     */
+    readDir(path) { }
+    /**
+     * @description Deletes a file or directory
+     * @param {string} path - Path to the file or directory
+     * @return {boolean} success
+     */
+    remove(path) { }
+    /**
+     * @description Creates a directory
+     * @param {string} path - Path of the directory
+     * @return {boolean} success
+     */
+    mkDir(path) { }
+    /**
+     * @description Creates every missing directory in a path
+     * @param {string} path - Path of the directory
+     * @return {boolean} success
+     */
+    mkDirAll(path) { }
+    /**
+     * @description Deletes a file or directory
+     * @param {string} path - Path to the file or directory
+     * @param {string} newPath - New path to the file or directory
+     * @return {boolean} success
+     */
+    rename(path, newPath) { }
 }
