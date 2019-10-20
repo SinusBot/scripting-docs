1e9343f7b8d9a8b70710facff8a875abf7a5146e
Jonas Bögle <jonas@boegle.me>
2019-10-20 20:13:26 +0200

    fix ClientServerGroupEvent capitalization

--- api/types/ClientServerGroupEvent.js
+++ api/types/ClientServerGroupEvent.js
@@ -1,6 +1,6 @@
 /**
  * @interface
- * @typedef {object} ClientServergroupEvent
+ * @typedef {object} ClientServerGroupEvent
  * @property {Client} client - Client that has been added / removed
  * @property {Client} invoker - Client that added client to the group
  * @property {ServerGroup} serverGroup - Server Group
