def6bb33f0bf017d634c08d309756e528f2cb3ab
Relentless <damnrelentless.yt@gmail.com>
2020-05-27 18:57:29 +0200

    Changed datatypes of channels in MoveInfo
    Signed-off-by: DAmNRelentless <damnrelentless.yt@gmail.com>

--- api/types/MoveInfo.js
+++ api/types/MoveInfo.js
@@ -1,8 +1,8 @@
 /**
  * @interface
  * @typedef {object} MoveInfo
- * @property {Channel} [fromChannel] - Old channel (or null if the client just got online/changed visibility)
- * @property {Channel} [toChannel] - New channel (or null if the client just went offline/changed visibility)
+ * @property {Channel} [fromChannel] - Old channel (or undefined if the client just got online/changed visibility)
+ * @property {Channel} [toChannel] - New channel (or undefined if the client just went offline/changed visibility)
  * @property {Client} client - Client that was moved
  * @property {Client} invoker - Client that invoked the move
  * @property {string} [message] - move/ban/kick message (TS3; since 1.0.0-beta.6)
